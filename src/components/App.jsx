7bc0b0dcaa5e58057691c837260884c31647347c
Jakub Antoniak <antonipomorzanin@gmail.com>
2023-03-23 21:20:32 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import user from './user.json';
 import stats from './data.json';
 import friends from './friends.json';
 import transactions from './transactions.json';
-import { Profile } from '../components/Profile/Profile';
+import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
 import { TransactionHistory } from './Transactions/TransactionHistory';
